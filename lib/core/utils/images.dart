62b7e298b605c535bbbe5907f63bdbe72f48aa37
Daly <monokakoman@gmail.com>
2024-08-31 11:40:19 +0300

    Add Assets and Styles for Responsive Design
    This commit introduces a comprehensive set of assets and styles to enhance the
    responsiveness and visual consistency of the application. New fonts, including
    Poppins in various weights (Regular, Medium, SemiBold, Bold, ExtraBold), have
    been added to the assets directory. Additionally, a collection of images has
    been included to support the application's graphical requirements.
    
    To facilitate the use of these assets, a new Dart file, `images.dart`, has been
    created under `lib/core/utils`, which centralizes the paths to all image assets,
    making them easily accessible throughout the application. Furthermore,
    `styles.dart` has been added to define a set of responsive text styles using
    the Poppins font family, ensuring that text elements adapt appropriately to
    different screen sizes.
    
    The `pubspec.yaml` file has been updated to include references to the new
    assets and fonts, ensuring they are properly bundled with the application. This
    commit lays the foundation for a more polished and adaptable user interface.

---
+++ lib/core/utils/images.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesApple
+  /// assets/images/apple.png
+  static const String imagesApple = "assets/images/apple.png";
+
+  /// Assets for imagesFacebook
+  /// assets/images/facebook.png
+  static const String imagesFacebook = "assets/images/facebook.png";
+
+  /// Assets for imagesGoogle
+  /// assets/images/google.png
+  static const String imagesGoogle = "assets/images/google.png";
+
+  /// Assets for imagesPulse
+  /// assets/images/Pulse.png
+  static const String imagesPulse = "assets/images/Pulse.png";
+
+  /// Assets for imagesTheme Logo Removed
+  /// assets/images/theme Logo Removed.png
+  static const String imagesThemeLogoRemoved =
+      "assets/images/theme Logo Removed.png";
+
+  /// Assets for imagesTheme logo
+  /// assets/images/theme logo.png
+  static const String imagesThemelogo = "assets/images/theme logo.png";
+
+  /// Assets for imagesVectorLogin
+  /// assets/images/vector_login.png
+  static const String imagesVectorLogin = "assets/images/vector_login.png";
+}
