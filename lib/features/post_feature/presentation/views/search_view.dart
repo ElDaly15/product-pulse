c21492e7388f983326b3bdafcc24bca479c11b42
Daly <monokakoman@gmail.com>
2024-09-03 11:13:11 +0300

    Refactor SearchView to support search type selection and dynamic content display
    - Convert SearchView from StatelessWidget to StatefulWidget to manage state for search type selection.
    - Introduce a state variable `index` to track the selected search type (Users or Products).
    - Add a row of buttons for selecting search type, each button updates the `index` state and triggers a rebuild.
    - Conditionally display search prompts and content based on the selected search type.
    - Update the SliverList delegate to display UserProfileItem for user search and PostItem for product search based on the `index` state.
    - Adjust UI elements to accommodate the new search type selection and dynamic content changes.

--- lib/features/post_feature/presentation/views/search_view.dart
+++ lib/features/post_feature/presentation/views/search_view.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:product_pulse/core/utils/styles.dart';
 import 'package:product_pulse/core/widgets/custom_search_text_field.dart';
+import 'package:product_pulse/features/post_feature/data/models/select_item_model.dart';
+import 'package:product_pulse/features/post_feature/presentation/views/widgets/item_btn_for_tab_bar.dart';
+import 'package:product_pulse/features/post_feature/presentation/views/widgets/post_item.dart';
 
 import 'package:product_pulse/features/post_feature/presentation/views/widgets/user_profile_item.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends StatefulWidget {
   const SearchView({super.key});
+
+  @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +25,67 @@ class SearchView extends StatelessWidget {
         slivers: [
           const SliverToBoxAdapter(
             child: SizedBox(
-              height: 60,
+              height: 70,
             ),
           ),
           SliverToBoxAdapter(
             child: Text(
-              'Search For Any User ',
+              'Select Search Type ',
               style: Style.font22Bold(context),
             ),
           ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 10,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {
+                    index = 0;
+                    setState(() {});
+                  },
+                  child: ItemBtn(
+                    isChecked: index == 0,
+                    selectItemModel:
+                        SelectItemModel(title: 'Users', iconData: Icons.person),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    index = 1;
+                    setState(() {});
+                  },
+                  child: ItemBtn(
+                    isChecked: index == 1,
+                    selectItemModel: SelectItemModel(
+                        title: 'Products', iconData: Icons.shopify_sharp),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 20,
+            ),
+          ),
+          index == 0
+              ? SliverToBoxAdapter(
+                  child: Text(
+                    'Search For Any User ',
+                    style: Style.font22Bold(context),
+                  ),
+                )
+              : SliverToBoxAdapter(
+                  child: Text(
+                    'Search For Any Product ',
+                    style: Style.font22Bold(context),
+                  ),
+                ),
           const SliverToBoxAdapter(
             child: SizedBox(
               height: 15,
@@ -52,12 +114,12 @@ class SearchView extends StatelessWidget {
             ),
           ),
           SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) {
+            delegate: SliverChildBuilderDelegate((context, i) {
               return Padding(
-                padding: index == 3
+                padding: i == 3
                     ? const EdgeInsets.only(bottom: 100)
                     : const EdgeInsets.only(bottom: 16),
-                child: const UserProfileItem(),
+                child: index == 0 ? const UserProfileItem() : const PostItem(),
               );
             }, childCount: 4),
           ),
